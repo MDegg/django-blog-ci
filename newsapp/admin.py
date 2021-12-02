cd4589b9e5f25f026904c5785498209095e06543
MDegg <matt.degg@outlook.com>
2021-12-02 11:09:13 +0000

    updated admin to allow comment approval and post create

--- newsapp/admin.py
+++ newsapp/admin.py
@@ -1,13 +1,24 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    list_display = ('title', 'slug', 'status', 'created_on', 'category')
+    search_fields = ['title','content','category']
+    prepopulated_fields = {'slug':('title',)}
+    list_filter = ('status','created_on','category')
     summernote_fields = ('content')
 
+@admin.register(Comment)
+class Comment(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on','approved')
+    list_filter = ('approved','created_on',)
+    search_fields = ('name','email','body','category')
+    actions = ['approve_comments']
 
-
+    def approve_comments(self,request,queryset):
+        queryset.update(approved=True)
 
 # Register your models here.
