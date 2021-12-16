a8406c259fd7f27049eaff7b3bdfc6bc4b6f0fc4
MDegg <matt.degg@outlook.com>
2021-12-16 13:57:29 +0000

    installed allauth and created login and login ability

--- DjangoNews/settings.py
+++ DjangoNews/settings.py
@@ -42,12 +42,22 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.staticfiles',
     'django_summernote',
     'newsapp',
     'cloudinary_storage',
     'cloudinary'
+    
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
